8d45647ae7a19cadb2a484d4e7e46254aa7b606f
AlfieHO <alfieho.5in@gmail.com>
2026-05-25 11:27:41 +0800

    Add 67

--- students/Alfie/scripts/graffiti.js
+++ students/Alfie/scripts/graffiti.js
@@ -1,5 +1,43 @@
-console.log('piyan');
-
+/*
+* HTML elements
+*/
 const graffitiCanvas = document.getElementById("my-graffiti");
+const surface = graffitiCanvas.getContext("2d");
+const cleanButton = document.getElementById("clean");
 
-const surface = graffitiCanvas.getContext("2d")
+/*
+ Graffiti stlye.
+
+  Hola there!!
+*/
+surface.lineWidth = 13;
+surface.lineJoin = "round";
+surface.strokeStyle = "red";
+
+/*
+* Shapes
+*/
+function shapes() {
+    surface.rect(150, 200, 100, 80);
+    surface.stroke();
+    surface.beginPath();
+    surface.moveTo(200, 50);
+    surface.lineTo(150, 200);
+
+    surface.moveTo(250, 50);
+    surface.lineTo(350, 50);
+    surface.moveTo(350, 50);
+    surface.lineTo(270, 300);
+
+    surface.closePath();
+    surface.stroke();
+}
+shapes();
+
+/*
+* clean up
+*/
+function cleanCanvas() {
+    surface.clearRect(0, 0, 400, 400);
+}
+cleanButton.addEventListener("click", cleanCanvas);
