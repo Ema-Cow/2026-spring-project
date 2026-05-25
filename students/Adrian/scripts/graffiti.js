23df465fc9eaea2533be9ec158ad1589678fd3c0
Adrian <suadrian.5in@gmail.com>
2026-05-25 11:27:40 +0800

    finished clear button

--- students/Adrian/scripts/graffiti.js
+++ students/Adrian/scripts/graffiti.js
@@ -1,2 +1,38 @@
+/* 
+*HTML elements
+*/
 const graffitiCanvas = document.getElementById("my-graffiti");
-const sutface = graffitiCanvas.getContext("2d");
+const surface = graffitiCanvas.getContext("2d");
+const cleanButton = document.getElementById("clean");
+
+/*
+*Graffiti style
+*/
+
+surface.lineWidth = 13;
+surface.lineJoin="round";
+surface.strokeStyle="red";
+/*
+*shapes
+ */
+function shapes(){
+
+
+surface.rect(150,200,100,100);
+surface.stroke();
+surface.moveTo(200,50);
+surface.lineTo(150,200);
+surface.closePath();
+surface.stroke();
+
+}
+shapes();
+/*
+ * Clean
+ */
+function cleanCanvas(){
+    surface.clearRect(0,0,400,400)
+
+}
+
+cleanButton.addEventListener("click",cleanCanvas);
